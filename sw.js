45f19e9c2d68b5162ffa6eea8b40b3f11d620a32
gastonm3112 <gastonm3112@gmail.com>
2021-01-09 12:05:40 -0300

    Cache creada dentro de sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+//Colocando Cache
+const CACHE_NAME = "v1_cache_contador_vue";
+const urlsToCache = [
+    "./",
+    "./img/favicon.png",
+    "./img/icon_32.png",
+    "./img/icon_64.png",
+    "./img/icon_128.png",
+    "./img/icon_256.png",
+    "./img/icon_512.png",
+    "./img/icon_1024.png",
+    "./js/main.js",
+    "./js/mountApp.js",
+    "https://unpkg.com/vue@next",
+    "./css/style.css",
+    "https://necolas.github.io/normalize.css/8.0.1/normalize.css"
+]
